951bcc06486c4e7a5a5232f8e92895d292d39ac1
YUUKIToriyama <43945424+YUUKIToriyama@users.noreply.github.com>
2021-02-05 04:57:44 +0900

    お知らせダイアログを追加した

--- src/MenuBar.jsx
+++ src/MenuBar.jsx
@@ -4,11 +4,18 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import {
 	AppBar,
+	Badge,
+	Button,
 	Toolbar,
 	Typography,
-	IconButton
+	IconButton,
+	Dialog,
+	DialogTitle,
+	DialogContent,
+	DialogContentText,
+	DialogActions
 } from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
+import { Menu, Notifications } from "@material-ui/icons";
 import SideMenu from "./SideMenu";
 
 const useStyles = makeStyles(theme => ({
@@ -26,6 +33,8 @@ const useStyles = makeStyles(theme => ({
 const MenuBar = () => {
 	const sidemenuRef = React.useRef();
 	const [menuopen, setMenuopen] = React.useState(false);
+	const [notificationOpen, setNotificationOpen] = React.useState(false);
+	const [numBadge, setNumBadge] = React.useState(1);
 
 	const classes = useStyles();
 	return (
@@ -33,11 +42,43 @@ const MenuBar = () => {
 			<AppBar position="static">
 				<Toolbar>
 					<IconButton edge="start" className={classes.menuButton} color="inherit">
-						<MenuIcon onClick={() => setMenuopen(true)} />
+						<Menu onClick={() => setMenuopen(true)} />
 					</IconButton>
 					<Typography variant="h6" className={classes.title}>
 						maptileGlue
-				</Typography>
+					</Typography>
+
+					<IconButton aria-label="show 17 new notifications" color="inherit" onClick={() => setNotificationOpen(true)}>
+						<Badge badgeContent={numBadge} color="secondary">
+							<Notifications />
+						</Badge>
+					</IconButton>
+					<Dialog
+						open={notificationOpen}
+						onClose={() => {
+							setNotificationOpen(false);
+							setNumBadge(0);
+						}}
+						aria-labelledby="お知らせ"
+						aria-describedby="お知らせ画面"
+					>
+						<DialogTitle id="notification-dialog-title">お知らせ</DialogTitle>
+						<DialogContent>
+							<DialogContentText id="notification-dialog-content">
+								maptileGlueがリリースされました！感想・プルリクお待ちしております！
+          					</DialogContentText>
+						</DialogContent>
+						<DialogActions>
+							<Button
+								onClick={() => {
+									setNotificationOpen(false);
+									setNumBadge(0);
+								}}
+								color="primary">
+								閉じる
+        					</Button>
+						</DialogActions>
+					</Dialog>
 				</Toolbar>
 			</AppBar>
 			<SideMenu ref={sidemenuRef} menuopen={menuopen} setMenuopen={setMenuopen} />
