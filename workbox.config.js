f179100f20b09b79bcbce1288b5725444e70d84e
YUUKIToriyama <github@toriyama.dev>
2022-07-29 23:52:40 +0900

    fix バンドルして生成したmain.jsのファイルサイズが大きく、キャッシュ対象から除外されていたのでファイルサイズ上限を4MBに設定

--- workbox.config.js
+++ workbox.config.js
@@ -2,33 +2,32 @@
 
 module.exports = {
 	// キャッシュ保持の対象となるディレクトリ
-	"globDirectory": "build/",
+	globDirectory: "build/",
 	// キャッシュするファイル
-	"globPatterns": [
-		"**/*.{js,json,css,jpg,png,html,txt}"
-	],
+	globPatterns: ["**/*.{js,json,css,jpg,png,html,txt}"],
 	// serviceWorkerの場所
-	"swDest": "build/sw.js",
+	swDest: "build/sw.js",
 	// ラインタイムキャッシュ
-	"runtimeCaching": [
-		{
-			urlPattern: /^https:\/\/fonts\.googleapis\.com/,
-			handler: 'StaleWhileRevalidate',
-			options: {
-				cacheName: 'google-fonts-stylesheets'
-			}
+	runtimeCaching: [{
+		urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+		handler: "StaleWhileRevalidate",
+		options: {
+			cacheName: "google-fonts-stylesheets",
 		},
-		{
-			urlPattern: /^https:\/\/fonts\.gstatic\.com/,
-			handler: 'CacheFirst',
-			options: {
-				cacheName: 'google-fonts-webfonts',
-				expiration: {
-					// GoogleFontsは7日間キャッシュする
-					maxAgeSeconds: 60 * 60 * 24 * 7,
-					maxEntries: 30
-				}
-			}
-		}
-	]
+	},
+	{
+		urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+		handler: "CacheFirst",
+		options: {
+			cacheName: "google-fonts-webfonts",
+			expiration: {
+				// GoogleFontsは7日間キャッシュする
+				maxAgeSeconds: 60 * 60 * 24 * 7,
+				maxEntries: 30,
+			},
+		},
+	},
+	],
+	// キャッシュ対象とするファイルのサイズ上限を4MBに設定
+	maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
 };
